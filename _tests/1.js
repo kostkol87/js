f61244eb6447a3d94a3dd0256be04f91b423dfc2
Konstantin Kolesov <Konstantin_Kolesov@epam.com>
2016-09-27 13:27:12 +0300

    added few tests

--- _tests/1.js
+++ _tests/1.js
@@ -1,7 +1,21 @@
+"use strict";
+
+function add(a, b) {
+    return a + b;
+};
+
 describe("Jasmine", function () {
+
     it("makes testing JavaScript awesome!", function () {
         console.log("HELOOOOO");
         expect(true).toBe(true);
     });
-
+    it("addiction equal", function () {
+        console.log("ADD TEST");
+        expect(add(20, 15)).toBe(35)
+    });
+    it("addiction not equal", function () {
+        console.log("ADD TEST");
+        expect(add(10, 2)).not.toBe(10)
+    });
 });
